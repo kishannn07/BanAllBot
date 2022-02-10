63456c022c57d862c4faa72cefa3c655f81dfae5
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-02-11 02:54:01 +0530

    Update bot.py

--- Code/bot.py
+++ Code/bot.py
@@ -1,1 +1,16 @@
 from Code import var
+
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+bot=Client(
+           ":memory:",
+           api_id=var.API_KEY,
+           api_hash=var.APP_HASH,
+           bot_token=var.TOKEN
+)
